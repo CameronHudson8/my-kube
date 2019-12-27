056491645b3213c4521903fc941a1b871956f7b6
Cameron Hudson <35085463+CameronHudson8@users.noreply.github.com>
2019-12-27 12:14:02 -0800

    Can attach billing account, enable APIs, create cluster, install Tiller, and install ingress.

--- setup.sh
+++ setup.sh
@@ -3,17 +3,80 @@
 # Exit if any command fails
 set -e
 
-# Log in
-gcloud auth login cameronhudson8@gmail.com
+echo "LOGGING IN"
+ACCOUNT_EMAIL="cameronhudson8@gmail.com"
+gcloud auth login $ACCOUNT_EMAIL
 
-# Create the project at set it as active
-echo "PROJECT CREATION"
-read -p "Enter project ID (must be globally unique): " PROJECT_ID 
-read -p "Enter project name: " PROJECT_NAME
-gcloud projects create $PROJECT_ID --name $PROJECT_NAME
+echo "CHECKING PROJECT EXISTENCE"
+PROJECT_ID="cameronhudson8-my-kube"
+PROJECT_NAME="my-kube"
+if gcloud projects list | grep -q $PROJECT_ID 
+then
+    echo "PROJECT ALREADY EXISTS"
+else
+    echo "CREATING PROJECT"
+    gcloud projects create $PROJECT_ID --name $PROJECT_NAME
+fi
 
-# Create a cluster
-echo "CLUSTER CREATION"
-read -p "Enter project ID (must be globally unique): " PROJECT_ID 
-gcloud --project=$PROJECT_ID container clusters create
+echo "ATTACHING BILLING ACCOUNT"
+gcloud components install alpha -q > /dev/null
+BILLING_ACCOUNT_ID=$(gcloud alpha billing accounts list | awk 'NR == 2 { print $1 }')
+gcloud alpha billing projects link $PROJECT_ID --billing-account $BILLING_ACCOUNT_ID > /dev/null
 
+echo "ENABLING NECESSARY APIS"
+gcloud --project=$PROJECT_ID services enable compute.googleapis.com
+gcloud --project=$PROJECT_ID services enable container.googleapis.com
+gcloud --project=$PROJECT_ID services enable containerregistry.googleapis.com
+gcloud --project=$PROJECT_ID services enable iam.googleapis.com
+gcloud --project=$PROJECT_ID services enable iamcredentials.googleapis.com
+gcloud --project=$PROJECT_ID services enable pubsub.googleapis.com
+
+CLUSTER_NAME="my-cluster"
+ZONE_NAME="us-west1-c"
+NUM_NODES="2"
+USE_PREEMPTIBLE="true"
+MACHINE_TYPE_NAME="g1-small"
+echo "CHECKING CLUSTER EXISTENCE"
+if gcloud container clusters list | grep $CLUSTER_NAME -q
+then
+    echo "CLUSTER ALREADY EXISTS"
+else
+    echo "CREATING CLUSTER"
+    gcloud --project=$PROJECT_ID container clusters create $CLUSTER_NAME \
+        --no-enable-basic-auth \
+        --no-issue-client-certificate \
+        --enable-ip-alias \
+        --metadata disable-legacy-endpoints=true \
+        --zone=$ZONE_NAME \
+        --num-nodes=$NUM_NODES \
+        $( (( $USE_PREEMPTIBLE == true )) && printf %s '--preemptible' ) \
+        --machine-type=$MACHINE_TYPE_NAME \
+        -q
+    gcloud --project=$PROJECT_ID config set compute/zone $ZONE_NAME -q
+fi
+
+echo "CHECKING TILLER EXISTENCE"
+if kubectl -n kube-system get pods | grep tiller -q
+then
+    echo "TILLER ALREADY INSTALLED"
+else
+    echo "INSTALLING TILLER"
+    kubectl create serviceaccount --namespace kube-system tiller
+    kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
+    helm init --service-account tiller
+fi
+
+echo "CHECKING INGRESS EXISTENCE"
+if kubectl -n ingress get pods | grep nginx -q
+then
+    echo "INGRESS ALREADY INSTALLED"
+else
+    echo "INSTALLING NGINX INGRESS"
+    helm install \
+      --namespace ingress \
+      --name nginx-ingress \
+      stable/nginx-ingress \
+      --set rbac.create=true \
+      --set controller.publishService.enabled=true \
+      --set controller.replicaCount=2
+fi
